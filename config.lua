e1a6b00a172e12582fb89f222ccf5701bfac00c2
MunkeyHacker <florisvanderleij@outlook.com>
2026-03-22 17:34:40 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,4 @@
 local module = {}
-
 local HttpService = game:GetService("HttpService")
 
 module.meta = {
@@ -15,19 +14,11 @@ local file = folder .. game.PlaceId .. ".json"
 module.Custom = {}
 
 local function ensureFolder()
-
-    if not isfolder("MunkeyHub") then
-        makefolder("MunkeyHub")
-    end
-
-    if not isfolder(folder) then
-        makefolder(folder)
-    end
-
+    if not isfolder("MunkeyHub") then makefolder("MunkeyHub") end
+    if not isfolder(folder) then makefolder(folder) end
 end
 
 function module.save()
-
     ensureFolder()
 
     local data = {
@@ -36,81 +27,64 @@ function module.save()
         Custom = module.Custom
     }
 
-    for k,v in pairs(Toggles) do
-        data.Toggles[k] = v.Value
+    if getgenv().Toggles then
+        for k,v in pairs(getgenv().Toggles) do
+            if v and v.Value ~= nil then
+                data.Toggles[k] = v.Value
+            end
+        end
     end
 
-    for k,v in pairs(Options) do
-
-        if typeof(v.Value) == "Color3" then
-
-            data.Options[k] = {
-                __type = "Color3",
-                r = v.Value.R,
-                g = v.Value.G,
-                b = v.Value.B
-            }
-
-        else
-            data.Options[k] = v.Value
+    if getgenv().Options then
+        for k,v in pairs(getgenv().Options) do
+            if v and v.Value ~= nil then
+                if typeof(v.Value) == "Color3" then
+                    data.Options[k] = {__type="Color3",r=v.Value.R,g=v.Value.G,b=v.Value.B}
+                else
+                    data.Options[k] = v.Value
+                end
+            end
         end
-
     end
 
     writefile(file,HttpService:JSONEncode(data))
-
 end
 
 function module.load()
-
     if not isfile(file) then return end
-
     local data = HttpService:JSONDecode(readfile(file))
 
-    if data.Toggles then
+    if getgenv().Toggles and data.Toggles then
         for k,v in pairs(data.Toggles) do
-            if Toggles[k] then
-                Toggles[k]:SetValue(v)
+            if getgenv().Toggles[k] then
+                getgenv().Toggles[k]:SetValue(v)
             end
         end
     end
 
-    if data.Options then
+    if getgenv().Options and data.Options then
         for k,v in pairs(data.Options) do
-
-            if Options[k] then
-
+            if getgenv().Options[k] then
                 if type(v) == "table" and v.__type == "Color3" then
-
-                    Options[k]:SetValue(
-                        Color3.new(v.r,v.g,v.b)
-                    )
-
+                    getgenv().Options[k]:SetValue(Color3.new(v.r,v.g,v.b))
                 else
-                    Options[k]:SetValue(v)
+                    getgenv().Options[k]:SetValue(v)
                 end
-
             end
-
         end
     end
 
     module.Custom = data.Custom or {}
-
 end
 
 function module.init(ctx)
-
     local box = ctx.box
-
     box:AddButton("Save Config",function()
         module.save()
     end)
-
     box:AddButton("Load Config",function()
         module.load()
     end)
-
 end
 
 return module
